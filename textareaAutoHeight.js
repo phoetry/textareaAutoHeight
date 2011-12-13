cb6bea1072f5e3ee4742a20a626cc49a3c83884e
phoetry <anymango@gmail.com>
2011-12-13 23:33:42 +0800

    improve performance and try to fix a Chinese input bug

--- textareaAutoHeight.js
+++ textareaAutoHeight.js
@@ -1,43 +1,37 @@
 /**
-  * jQuery TAH Plugin Beta v0.3
+  * jQuery TAH Plugin
   * Using for Textarea-Auto-Height
-  * @Update: October 18, 2011
+  * @Version: 0.4
+  * @Update: December 13, 2011
   * @Author: Phoetry (http://phoetry.me)
   * @Url: http://phoetry.me/archives/tah.html
   **/
 ~function($){
 $.fn.tah=function(opt){
 	opt=$.extend({
-		moreSpace:15,
+		moreSpace:10,
 		maxHeight:600,
 		animateDur:200
 	},opt);
-	return this.each(function(){
-		if(!$.nodeName(this,'textarea'))return;
-		var ta=$(this).css({resize:'none',overflowY:'hidden'}),
-			css=(function(){
-				var css={};
-				'width fontSize fontFamily lineHeight wordWrap wordBreak whiteSpace letterSpacing'
-				.replace(/[^ ]+/g,function(s){css[s]=ta.css(s)});
+	return this.each(function(i,t){
+		if(!$.nodeName(t,'textarea'))return;
+		var ta=$(t).css({resize:'none',overflowY:'hidden'}),
+			_ta=ta.clone().attr({id:'',name:'',tabIndex:-1}).css(function(css){
+				$.each('width fontSize fontFamily lineHeight wordWrap wordBreak whiteSpace letterSpacing'.split(' '),function(i,t){css[t]=ta.css(t)});
 				return $.extend(css,{
 					width:ta.width()*1.5,
 					position:'absolute',
-					left:-9999,
+					left:-9e5,
 					height:0
 				});
-			}()),
-			_ta=ta.clone().css(css).attr({id:'',name:'',tabIndex:-1}),
-			stCache,stCur,valCur,defHeight=ta.height(),
+			}({})),
+			valCur,stCur,stCache,defHeight=ta.height(),
 			autoHeight=function(){
-				valCur=ta.val();
-				_ta.val(valCur).scrollTop(9999);
-				stCur=Math.max(defHeight,_ta.scrollTop())+(valCur?opt.moreSpace:0);
-				stCur==stCache||(
-					stCache=stCur,
-					stCur<opt.maxHeight?ta.stop().animate({height:stCur},opt.animateDur):ta.css({overflowY:''})
-				);
+				_ta.val(valCur=ta.val()).scrollTop(9e5);
+				stCur=Math.max(defHeight,_ta.scrollTop())+(valCur&&opt.moreSpace);
+				stCur==stCache?0:(stCache=stCur)<opt.maxHeight?ta.stop().animate({height:stCur},opt.animateDur):ta.css({overflowY:''});
 			};
-		ta.after(_ta).bind('blur focus input change propertychange',autoHeight);
+		ta.after(_ta).on('blur focus input change propertychange',autoHeight);
 	});
 };
 }(jQuery);
